50af4aecd076dc01d42d340836126d0bb7edb80b
ESTHER EDUARDA ANDRADE DOS SANTOS <00001108045297sp@aluno.educacao.sp.gov.br>
2024-10-18 11:16:25 -0300

    h

--- script.js
+++ script.js
@@ -4,9 +4,21 @@ const inputPesquisa = document.querySelector('.input-pesquisa');
 
 // Adiciona um evento de clique ao botão "X"
 btnFechar.addEventListener('click', () => {
-    // Limpa o valor do input e restaura o placeholder
     inputPesquisa.value = ''; // Limpa o valor
     inputPesquisa.placeholder = 'Buscar...'; // Restaura o placeholder
 });
 
-// Aqui você pode incluir a funcionalidade do carrinho, se necessário.
+function togglePopup() {
+    const popup = document.getElementById('popup');
+    console.log("Popup toggled"); // Para verificar se a função é chamada
+    popup.style.display = (popup.style.display === 'block') ? 'none' : 'block';
+}
+
+// Adiciona um evento de clique ao ícone de opções
+const iconeOpcoes = document.querySelector('.icone-opcoes');
+iconeOpcoes.addEventListener('click', togglePopup);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none'; // Garante que o popup esteja escondido ao carregar a página
+});
