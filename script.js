ec9fd9d7ee796e3a77fcda9f69abab7c0d08bfbb
ESTHER EDUARDA ANDRADE DOS SANTOS <00001108045297sp@aluno.educacao.sp.gov.br>
2024-10-18 15:25:32 -0300

    j

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// Seleciona o botão "X" e o input da barra de pesquisa
 const btnFechar = document.querySelector('.btn-fechar');
 const inputPesquisa = document.querySelector('.input-pesquisa');
 
-// Adiciona um evento de clique ao botão "X"
+
 btnFechar.addEventListener('click', () => {
-    inputPesquisa.value = ''; // Limpa o valor
-    inputPesquisa.placeholder = 'Buscar...'; // Restaura o placeholder
+    inputPesquisa.value = ''; 
+    inputPesquisa.placeholder = 'Buscar...'; 
 });
 
 function togglePopup() {
     const popup = document.getElementById('popup');
-    console.log("Popup toggled"); // Para verificar se a função é chamada
-    popup.style.display = (popup.style.display === 'block') ? 'none' : 'block';
+    if (popup.style.display === 'none' || popup.style.display === '') {
+        popup.style.display = 'block'; 
+    } else {
+        popup.style.display = 'none'; 
+    }
 }
 
-// Adiciona um evento de clique ao ícone de opções
-const iconeOpcoes = document.querySelector('.icone-opcoes');
-iconeOpcoes.addEventListener('click', togglePopup);
-
-document.addEventListener("DOMContentLoaded", () => {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none'; // Garante que o popup esteja escondido ao carregar a página
-});
+function togglePesquisaPopup() {
+    const popupPesquisa = document.getElementById('popup-pesquisa');
+    popupPesquisa.style.display = popupPesquisa.style.display === 'block' ? 'none' : 'block';
+}
